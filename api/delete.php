571ae757b4e5f7d11a3d70629ee3c1d9bf87ac91
Matheus Nascimento <mnsergio59@gmail.com>
2022-05-09 23:24:48 -0300

    first commit

---
+++ api/delete.php
@@ -0,0 +1,27 @@
+<?php 
+require('../config.php');
+
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+if($method === 'delete'){
+    
+    parse_str(file_get_contents('php://input'), $input);
+
+    $id = (!empty($input['id'])) ? $input['id'] : null;
+
+    $id = filter_var($id);
+    
+
+    if($id){
+
+            $sql = $pdo->prepare("DELETE FROM notes WHERE id = :id");
+            $sql->bindValue(':id', $id);
+            $sql->execute();
+
+    }else{
+        $array['error'] = 'ID não preenchido';
+    }
+}else {
+    $array['error'] = 'Metodo incorreto(DELETE somente)';
+};
+
+require('../return.php');
---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$db_name = 'devsnotes';
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+
+$pdo = new PDO("mysql:dbname=$db_name;host=$db_host",$db_user, $db_pass);
+
+$array = [
+    'error' => '',
+    'result' => []
+];
